23404eeafa5d3d7d95ce338809ac50a6a0143b9d
Ba5eem <baseem@gmail.com>
2017-12-16 12:36:51 -1000

    search of titles set

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {loadArtworks} from '../../actions/artworks';
-import SearchInput, {createFilter} from 'react-search-input'
 import _ from 'lodash';
 import './style.css';
  
@@ -11,14 +10,23 @@ class Search extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      query: ''
+      query: [],
+      input: []
     }
     this.search = this.search.bind(this)
   }
 
     search(e) {
+      let data = this.props.artworks;
       let query = e.target.value;
-      this.setState({query: query})
+      let m = data.map((elem)=>{
+        let x = _.mapValues(elem, _.method('toLowerCase'));
+        if(query.length > 4){
+          if(x.title.match(query)){
+            this.setState({input: [x]})
+          }
+        }
+      })    
   }
 
 
@@ -26,19 +34,20 @@ class Search extends Component {
 
 
    render () {
+    let res = this.state.input;
+    console.log(res);
+    //console.log(data);
     let data = this.props.artworks;
-    console.log(data);
-    let query = this.state.query;
-    let res = data.filter((elem)=>{
-      let artworks = _.mapValues(elem, _.method('toLowerCase'));
-      let x = query.toLowerCase();
-      console.log(x);
-      console.log(elem.title === x);
-      return artworks.title === x;
-    })
+    // let res = data.filter((elem)=>{
+    //   let artworks = _.mapValues(elem, _.method('toLowerCase'));
+    //   //let x = query.toLowerCase();
+    //   console.log(artworks);
+    //   //console.log(elem.title === x);
+    //   ///return artworks.title === x;
+    // })
     
 
-    console.log(res);
+    //console.log(res);
 
 
     return (
@@ -47,6 +56,7 @@ class Search extends Component {
         <img src="http://bit.ly/2ygeReT"  alt="icon"  className="search-icon-temp"/>
         <input type="text" placeholder="Search" className="search-input-temp" onChange={this.search}/>
       </div>
+        
       {res.map((term,i)=>{
         return(
             <p key={i} className="search-result">{term.title}</p>
